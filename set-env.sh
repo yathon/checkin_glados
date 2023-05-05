99ecf3707049495dc986bd16ccfbd8147f724c63
Yathon <1134791004@qq.com>
2023-05-05 17:35:09 +0800

    checkin batch

--- set-env.sh
+++ set-env.sh
@@ -4,6 +4,6 @@ if [ -f requirements.txt ]; then
   pip3 install -r requirements.txt
 fi
 
-for key in $ (compgen -v secrets_); do
-  export $ {key/secrets_/}=$ { !key}
+for key in $(compgen -v secrets_); do
+  export ${key/secrets_/}=${ !key }
 done
